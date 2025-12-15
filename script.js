8a57416b2f12c1b51e742df8ce03c09defc633bf
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-15 15:08:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function AddBook() {
+	const title = document.getElementById("title")
+	const author = document.getElementById("author")
+	const isbn = document.getElementById("isbn")
+	
+	const book_list = document.querySelector("#book-list")
+	const row = document.createElement("tr")
+	const title_cell = document.createElement("td")
+	const author_cell = document.createElement("td")
+	const isbn_cell = document.createElement("td")
+	
+	row.appendChild(title_cell)
+	row.appendChild(author_cell)
+	row.appendChild(isbn_cell)
+	title_cell.textContent = 
+}
+
