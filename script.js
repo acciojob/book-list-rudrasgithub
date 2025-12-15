8eac696524cc68d69b923d404da97a527ae818df
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-15 15:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 //your JS code here. If required.
+const title = document.getElementById("title")
+const author = document.getElementById("author")
+const isbn = document.getElementById("isbn")
+
 function AddBook() {
-	const title = document.getElementById("title")
-	const author = document.getElementById("author")
-	const isbn = document.getElementById("isbn")
-	
+	event.preventDefault();
 	const book_list = document.querySelector("#book-list")
 	const row = document.createElement("tr")
+	
 	const title_cell = document.createElement("td")
 	const author_cell = document.createElement("td")
 	const isbn_cell = document.createElement("td")
 	
+	title_cell.textContent = title.value;
+	author_cell.textContent = author.value;
+	isbn_cell.textContent = isbn.value;
+		
 	row.appendChild(title_cell)
 	row.appendChild(author_cell)
 	row.appendChild(isbn_cell)
-	title_cell.textContent = 
+	
+	book_list.appendChild(row)
 }
 
+document.getElementById("submit").addEventListener("click", AddBook)
